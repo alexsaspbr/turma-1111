a1d28f593c73b6279b90701488e7e0b367c2cb56
Ricard Roberg <ricardroberg@gmail.com>
2024-01-21 20:48:21 -0300

    ajustado metodos validarPalindromo e checarPalindromo

--- src/br/com/logicaprogramacao/Main.java
+++ src/br/com/logicaprogramacao/Main.java
@@ -1,16 +1,26 @@
 package br.com.logicaprogramacao;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(new Locale("pt", "BR"));
+
         Palindrono palindrono = new Palindrono();
 
         //essa deve retornar que é palindromo
-        palindrono.validarPalindromo("arara");
+        System.out.println(palindrono.validarPalindromo("arara"));
 
         //essa deve retornar que não palindromo
-        palindrono.validarPalindromo("carro");
+        System.out.println(palindrono.validarPalindromo("carro"));
+
+        //essa deve retornar que é palindromo
+        System.out.println(palindrono.validarPalindromo("omississimo"));
+
+        //essa deve retornar que é palindromo
+        System.out.println(palindrono.validarPalindromo("A cara rajada da jararaca"));
 
     }
 
--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -1,18 +1,26 @@
 package br.com.logicaprogramacao;
 
+import java.util.Locale;
+
 public class Palindrono {
 
     public String validarPalindromo(String palavra) {
+
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("O texto \"%s\" é um palíndromo", palavra);
+
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("O texto \"%s\" não é um palíndromo", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
+        String palavraPreparada = palavra.replaceAll(" ", "").toLowerCase();
+
+        StringBuilder revertePalavra = new StringBuilder(palavraPreparada).reverse();
+        String stringInvertida = revertePalavra.toString();
+
+        return stringInvertida.equals(palavraPreparada);
     }
 
 }
