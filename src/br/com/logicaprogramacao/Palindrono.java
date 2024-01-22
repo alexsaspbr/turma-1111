2791eba765d4145840192ad4db44aac77cf52e27
HandlezGZ <tirsoneto@hotmail.com>
2024-01-22 00:33:35 -0300

    metodo checarPalindromo implementado

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,15 +4,24 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
+        int low = 0;
+        int high = palavra.length() - 1;
+        String palavraLowerCase = palavra.toLowerCase();
+        while (low < high) {
+            if (palavraLowerCase.charAt(low) != palavraLowerCase.charAt(high)) {
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
     }
 
 }
