85cb17a6f0514277405932f0630df14424d47ea1
Pedro Souza <pedroantoniidm@gmail.com>
2024-01-22 20:51:47 -0300

    Resolução da atividade 3
    Além da lógica, coloquei o sout.

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,15 +4,19 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
+        int tamanho = palavra.length();
+        for (int i = 0; i < tamanho; i++) {
+            if (palavra.charAt(i) != palavra.charAt(tamanho - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
-
 }
