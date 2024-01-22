5be9337131c5763aade853e657d5d1983b102596
rafaelluis1 <rafael.luis.carvalho.maia@ccc.ufcg.edu.br>
2024-01-22 20:00:54 -0300

    implementação da classe checarPalindromo

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,15 +4,24 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
+        int tamanho = palavra.length();
+        int i = 0;
+        int j = tamanho-1;
+        while (i < j){
+            if (palavra.charAt(i) != palavra.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 
 }
