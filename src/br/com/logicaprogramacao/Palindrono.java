c8ef674838b861903895ec64704865f5c338f905
Wagner <marketing@letscode.com.br>
2024-01-22 19:05:45 -0300

    exercicio palindromo

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,15 +4,26 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo", palavra);
         }
+
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
+
+        int comprimento = palavra.length();
+
+        for(int i = 0; i < comprimento / 2; i++) {
+            //System.out.println("Comparando: " + palavra.charAt(i) + " e " + palavra.charAt(comprimento - 1 - i));
+            if (palavra.charAt(i) != palavra.charAt(comprimento - 1 -i)) {
+                return false;
+            }
+
+        }
+        return true;
     }
+
 
 }
