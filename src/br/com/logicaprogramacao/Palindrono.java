1d16a7e610ec37785de2bba9409f5ca684bedbdf
engdvj <eng.dvj@gmail.com>
2024-01-22 00:14:21 -0300

    resolução

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -1,18 +1,23 @@
 package br.com.logicaprogramacao;
-
 public class Palindrono {
-
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo",palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo",palavra);
         }
     }
-
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
-    }
+        int inicio = 0;
+        int fim = palavra.length() - 1;
 
+        while (inicio < fim) {
+            if (palavra.charAt(inicio) != palavra.charAt(fim)) {
+                return false;
+            }
+            inicio++;
+            fim--;
+        }
+        return true;
+    }
 }
