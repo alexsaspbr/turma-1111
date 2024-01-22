14d28b5a71a68f274b3abafd5366c5193e62c34a
Matheus Borges <matheusborgesmoraes1@gmail.com>
2024-01-22 17:51:53 -0300

    Criacao da funcao checarPalindromo e correcoes no metodo main

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,15 +4,24 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
+        int inicio = 0, fim = palavra.length() - 1;
+
+        while (fim > inicio) {
+            if (palavra.charAt(inicio) != palavra.charAt(fim)) {
+                return false;
+            }
+            inicio++;
+            fim--;
+        }
+
+        return true;
     }
 
 }
