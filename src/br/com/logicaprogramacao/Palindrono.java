3ffdbf485a0753cdeeb9d95aac5533d6bdd0e7a0
Biolasan <153777832+Biolasan@users.noreply.github.com>
2024-01-22 20:38:28 -0300

    Update Palindrono.java

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,17 +4,24 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
+        int comprimento = palavra.length();
+
+        for (int i = 0; i < comprimento / 2; i++) {
+
+        if (palavra.charAt(i) != palavra.charAt(comprimento - i - 1)) {
 
         return false;
-    }
+            }
+        }
 
+        return true;
+    }
 }
 
