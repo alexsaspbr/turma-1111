89825b1036b724a6ffeb984dfb1c0f3389b5754c
Yuyake23 <brunosamuel.orizona@gmail.com>
2024-01-22 19:29:53 -0300

    Exercicio 3 resolvido - Bruno Samuel

--- src/br/com/logicaprogramacao/Main.java
+++ src/br/com/logicaprogramacao/Main.java
@@ -2,16 +2,16 @@ package br.com.logicaprogramacao;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Palindrono palindrono = new Palindrono();
+		Palindrono palindrono = new Palindrono();
 
-        //essa deve retornar que é palindromo
-        palindrono.validarPalindromo("arara");
+		// essa deve retornar que é palindromo
+		System.out.println(palindrono.validarPalindromo("arara"));
 
-        //essa deve retornar que não palindromo
-        palindrono.validarPalindromo("carro");
+		// essa deve retornar que não palindromo
+		System.out.println(palindrono.validarPalindromo("carro"));
 
-    }
+	}
 
 }
--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -2,17 +2,26 @@ package br.com.logicaprogramacao;
 
 public class Palindrono {
 
-    public String validarPalindromo(String palavra) {
-        if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
-        } else {
-            return String.format("A palavra %s não é um palíndromo");
-        }
-    }
+	public String validarPalindromo(String palavra) {
+		if (checarPalindromo(palavra)) {
+			return String.format("A palavra %s é um palíndromo", palavra);
+		} else {
+			return String.format("A palavra %s não é um palíndromo", palavra);
+		}
+	}
 
-    private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
-    }
+	private boolean checarPalindromo(String palavra) {
+		char[] arrayPalavra = palavra.toCharArray();
+
+		for (int i = 0, x; i < arrayPalavra.length / 2; i++) {
+			x = arrayPalavra.length - 1 - i;
+
+			if (arrayPalavra[i] != arrayPalavra[x]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 
 }
