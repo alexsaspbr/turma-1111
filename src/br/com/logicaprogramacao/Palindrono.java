59645b15976be295f4ecd6b278e4805c3c4e8a87
Gabrielly <gabriellybsoares23@gmail.com>
2024-01-22 20:09:10 -0300

    Resolvendo verificador de palíndromo

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -2,17 +2,35 @@ package br.com.logicaprogramacao;
 
 public class Palindrono {
 
-    public String validarPalindromo(String palavra) {
+    public void validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            System.out.printf("A palavra %s é um palíndromo\n", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            System.out.printf("A palavra %s não é um palíndromo\n", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
-    }
+        palavra = palavra.trim().toLowerCase();
 
+        char[] inicial = palavra.toCharArray();
+        int qntCaracteres = inicial.length;
+
+        char[] contrario = new char[qntCaracteres];
+
+        for (int i = qntCaracteres - 1, j = 0; i >= 0; i--) {
+            if (Character.isLetter(inicial[i])) {
+                contrario[j] = inicial[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < qntCaracteres; i++) {
+            if (inicial[i] != contrario[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
