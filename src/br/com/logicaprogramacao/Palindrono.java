ef51607b110f4a87bd3a3fff4d3bbddb6125d320
ingridmmaciel <138080707+ingridmmaciel@users.noreply.github.com>
2024-01-22 20:06:48 -0300

    ajuste

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,9 +4,9 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra '%s' é um palíndromo");
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra '%s' não é um palíndromo");
         }
     }
 
