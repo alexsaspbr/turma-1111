89638113246bf25393197fcf5dde64f91b5b1f63
Edu <eduxz14@gmail.com>
2024-01-23 10:15:37 -0300

    Exercicio3 Finalizado

--- src/br/com/logicaprogramacao/Palindrono.java
+++ src/br/com/logicaprogramacao/Palindrono.java
@@ -4,15 +4,28 @@ public class Palindrono {
 
     public String validarPalindromo(String palavra) {
         if (checarPalindromo(palavra)) {
-            return String.format("A palavra %s é um palíndromo");
+            return String.format("A palavra %s é um palíndromo", palavra);
         } else {
-            return String.format("A palavra %s não é um palíndromo");
+            return String.format("A palavra %s não é um palíndromo", palavra);
         }
     }
 
     private boolean checarPalindromo(String palavra) {
-        //TODO implementar logica
-        return false;
-    }
+        palavra = palavra.toLowerCase();
 
+        char[] letra = palavra.toCharArray();
+
+        int inicio = 0;
+        int fim = letra.length - 1;
+
+        while (inicio < fim) {
+            if (letra[inicio] != letra[fim]) {
+                return false;
+            }
+            inicio++;
+            fim--;
+        }
+        return true;
+    }
 }
+
